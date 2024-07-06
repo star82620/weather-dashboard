4f9d3f6b6192bbe15318e3b6a17a07971e69e0f1
Ayu Huang <star82620@gmail.com>
2024-07-06 19:24:29 +0800

    feature: CurrentWeather layout and styled

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,5 +1,41 @@
-import { Wrapper } from "./styled";
+import {
+  City,
+  Country,
+  Content,
+  CurrentWeather,
+  Humidity,
+  Location,
+  Temperature,
+  Time,
+  Title,
+  WindSpeed,
+  Wrapper,
+} from "./styled";
 
 export default function Main() {
-  return <Wrapper>index</Wrapper>;
+  return (
+    <Wrapper>
+      <CurrentWeather>
+        <Time>07/06 18:13</Time>
+
+        <Location>
+          <City>Tainan</City>
+          <Country>Taiwan</Country>
+        </Location>
+
+        <Temperature $unit="℃">
+          <Content>32</Content>
+          <Title>Temperature</Title>
+        </Temperature>
+        <Humidity>
+          <Content>30%</Content>
+          <Title>Humidity</Title>
+        </Humidity>
+        <WindSpeed>
+          <Content>4 km/hr</Content>
+          <Title>Wind Speed</Title>
+        </WindSpeed>
+      </CurrentWeather>
+    </Wrapper>
+  );
 }
--- src/components/Main/styled.tsx
+++ src/components/Main/styled.tsx
@@ -1,6 +1,73 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+
+type Unit = {
+  $unit: string;
+};
+
+const TextAlignCenter = css`
+  text-align: center;
+`;
 
 export const Wrapper = styled.main`
-  background-color: #ccc;
   flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  align-items: center;
+  padding: 12px;
+`;
+
+export const CurrentWeather = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: flex-end;
+  align-content: flex-end;
+  flex-wrap: wrap;
+  gap: 8px 48px;
+  width: 100%;
+  max-width: 912px;
+  padding: 24px;
+  background-color: ${({ theme }) => theme.colors.grayLight};
+  border: 1px solid ${({ theme }) => theme.colors.light};
+  border-radius: ${({ theme }) => theme.borderRadius.common};
+`;
+
+export const Content = styled.p`
+  font-size: 24px;
+`;
+
+export const Title = styled.h3`
+  font-size: 14px;
+`;
+
+export const Time = styled.div`
+  width: 100%;
+`;
+
+export const Location = styled.div``;
+
+export const City = styled.p`
+  font-size: 32px;
+`;
+
+export const Country = styled.p``;
+
+export const Temperature = styled.div<Unit>`
+  ${TextAlignCenter}
+
+  ${({ $unit }) =>
+    $unit &&
+    css`
+      ${Content}::after {
+        content: "${$unit}";
+      }
+    `}
+`;
+
+export const Humidity = styled.div`
+  ${TextAlignCenter}
+`;
+
+export const WindSpeed = styled.div`
+  ${TextAlignCenter}
 `;
