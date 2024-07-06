637658e692ef373fff47ea1cfbc387db90c70bfd
Ayu Huang <star82620@gmail.com>
2024-07-06 15:50:21 +0800

    feature: SearchBar components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { ThemeProvider } from "styled-components";
 import theme from "./constants/style/defaultTheme";
 import { GlobalStyle } from "./constants/style/global";
+import SearchBar from "./components/SearchBar";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      weather
+      <SearchBar />
     </ThemeProvider>
   );
 }
